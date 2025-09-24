9c4140bf32e1580c1a7e52b0f6aecf67019be48d
ilredonald <donaldtchoupii@gmail.com>
2025-09-24 08:32:49 +0200

    header

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './core/components/header-component/header-component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [HeaderComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('projet');
