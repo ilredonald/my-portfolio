b7b7a2ed4ccd717cfb2701c3bf90a7c155d1c6ce
ilredonald <donaldtchoupii@gmail.com>
2025-09-27 17:00:45 +0200

    another commit

--- front-end/src/app/app.ts
+++ front-end/src/app/app.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './core/components/header-component/header-component';
+import { FooterComponent } from "./core/components/footer-component/footer-component";
+import { MainComponent } from "./core/components/main-component/main-component";
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, MainComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
